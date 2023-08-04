1968c300cec4e05489c22b3e756811fd25d4677c
김성원 <kswen0203@gmail.com>
2023-08-04 18:28:00 +0900

    feat: DependencyInjector 생성

---
+++ NoticeBoard/Application/DependencyInjector.swift
@@ -0,0 +1,30 @@
+//
+//  DependencyInjector.swift
+//  NoticeBoard
+//
+//  Created by 김성원 on 2023/08/04.
+//
+
+import Foundation
+import UIKit
+
+final class DependencyInjector {
+    
+    static let shared: DependencyInjector = DependencyInjector()
+    
+    private lazy var noticeBoardAPIFetcher: NoticeBoardAPIFetcherProtocol = NoticeBoardAPIFetcher()
+    
+    func makeNoticeBoardViewController() -> UIViewController {
+        let viewModel: NoticeBoardViewModelProtocol = NoticeBoardViewModel(noticeBoardAPIFetcher: noticeBoardAPIFetcher)
+        
+        return NoticeBoardViewController(viewModel: viewModel)
+    }
+    
+    func makeMenuSelectViewController(boardList: [Board],
+                                      parentableViewController: MenuSelectViewControllerListener) -> UIViewController {
+        let viewModel: MenuSelectViewModelProtocol = MenuSelectViewModel(boardList: boardList)
+        let viewController: MenuSelectViewController = MenuSelectViewController(viewModel: viewModel)
+        viewController.parentableViewController = parentableViewController
+        return viewController
+    }
+}
